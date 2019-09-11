4d2b4c8ef428ed3152b84b78bd9612a01dd4b64c
mikaellyvieira <mykkavieira067@gmail.com>
2019-09-11 14:49:30 -0300

    Contribuido por mikaellyvieira, turma 524-B

---
+++ cal.py
@@ -0,0 +1,8 @@
+var1 = float( input("Digite um numero: ") )
+var2 = float( input("Digite outro numero: ") )
+
+# Imprime o resultado das operações direto na função print
+print('Soma:             ', var1,'+',var2,' = ' , var1+var2)
+print('Subtração:        ', var1,'-',var2,' = ' , var1-var2)
+print('Multiplicação:    ', var1,'*',var2,' = ' , var1*var2)
+print('Divisão:          ', var1,'/',var2,' = ' , var1/var2)
--- calculadora.py
+++ calculadora.py
@@ -1,16 +1,16 @@
 #Complete as funcoes a seguir
 
 def soma(a, b):
-	#Insira o codigo aqui
-
+    print("A soma é", a + b)
 def subtrai(a, b):
-	#Insira o codigo aqui
-
+    print("A subtração é", a - b)
 def multiplica(a, b):
-	#Insira o codigo aqui
-
+    print("A multiplicação é", a * b)
 def divide(a, b):
-	#Insira o codigo aqui
+    if b == 0:
+            print ("O divisor não pode ser zero")
+    else :
+            print ("A divisão é", a/b)
 
 
 #Programa principal
